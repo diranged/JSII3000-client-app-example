616ab785ebcaf8163b9d5f0c779758fdbf28af48
Patrick Florek <patrick.florek@gmail.com>
2022-11-04 18:08:24 +0100

    fix: add transform ignore pattern
    > by default, babel excludes `node_modules`
    
    https://stackoverflow.com/questions/58086182/jest-ignore-all-node-modules-except-1-package

--- .projenrc.js
+++ .projenrc.js
@@ -5,9 +5,21 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'clientapp',
   scripts: {
-    "upgrade-coremodule": "yarn upgrade coremodule && cd node_modules/coremodule && yarn build",
+    'upgrade-coremodule': 'yarn upgrade coremodule && cd node_modules/coremodule && yarn build',
   },
-  gitignore: ["*.swp", "*.swo"],
+  gitignore: ['*.swp', '*.swo'],
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+      singleQuote: true,
+    },
+  },
+  jestOptions: {
+    jestConfig: {
+      transformIgnorePatterns: ['/node_modules\\/(?!coremodule).+\\.[tj]s$'],
+    },
+  },
 });
-project.addDeps("coremodule@https://github.com/diranged/JSII3000-coremodule-example#test");
+project.addDeps('coremodule@https://github.com/diranged/JSII3000-coremodule-example#test');
 project.synth();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,6 @@
-import { App } from 'aws-cdk-lib';
+import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { WidgetVpc } from 'coremodule/src/widgets/myWidget/vpc';
 import { MyStack } from '../src/main';
 
 test('Snapshot', () => {
@@ -8,4 +9,20 @@ test('Snapshot', () => {
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
+});
+
+test('Use widget from coremodule', () => {
+  // Given
+  const stack = new Stack();
+  const widget = new WidgetVpc(stack, 'WidgetVpc', {
+    name: 'test',
+    cidr: '10.0.0.0/24',
+  });
+
+  // When
+  const template = Template.fromStack(stack);
+
+  // Then
+  expect(widget).not.toBeNull();
+  template.hasResource('AWS::EC2::VPC', {});
 });
